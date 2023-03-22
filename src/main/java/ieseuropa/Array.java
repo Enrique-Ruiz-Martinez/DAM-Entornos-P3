d3bf0aeda6ed6b630871db7ebbb2f44197c4880a
dev <dev@debian>
2023-03-22 20:42:33 +0100

    Calcular media con ejemplo

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -1,10 +1,20 @@
 package main.java.ieseuropa;
 
 public class Array {
+	
+	private static float calcularMedia(int[] array) {
+		int total = 0;
+		for(int i=0;i<array.length;i++) {
+			total += array[i];
+		}
+		return (float)total/array.length;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
+		int[] array = {3,43,54,65,2,35,12,31};
+		System.out.println(calcularMedia(array));
 	}
 
 }

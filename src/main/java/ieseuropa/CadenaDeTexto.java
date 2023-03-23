d45d2133ba88579cbfd913ffe9d9e58661631a8b
PatriDominguez <122823076+PatriDominguez@users.noreply.github.com>
2023-03-23 13:14:33 +0100

    Ocurrencias de una letra completado

--- src/main/java/ieseuropa/CadenaDeTexto.java
+++ src/main/java/ieseuropa/CadenaDeTexto.java
@@ -2,6 +2,16 @@ package main.java.ieseuropa;
 
 public class CadenaDeTexto {
 	
+	private static int ocurrenciasLetra(String cadena) {
+		int contador = 0;
+		for (int i=0; i<cadena.length(); i++) {
+			if (cadena.charAt(i) == 'o') {
+				contador++;
+			}
+		}
+		return contador;
+	}
+	
 	private static int calcularLongitud(String cadena) {
 		return cadena.length();
 	}
@@ -9,6 +19,7 @@ public class CadenaDeTexto {
 	public static void main(String[] args) {
 		String cadena = "Hola mundo";
 		System.out.println("La longitud de la cadena es de " + calcularLongitud(cadena) + " letras");
+		System.out.println("El numero de ocurrencias de la letra 'o' es: " + ocurrenciasLetra(cadena));
 	}
 
 }

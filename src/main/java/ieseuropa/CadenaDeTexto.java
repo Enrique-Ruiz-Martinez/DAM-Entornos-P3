8965bcd4aa515f809830b7efeab2a7d8486b2f89
dev <dev@debian>
2023-03-24 16:34:58 +0100

    Metodos de strings diego

--- src/main/java/ieseuropa/CadenaDeTexto.java
+++ src/main/java/ieseuropa/CadenaDeTexto.java
@@ -1,5 +1,7 @@
 package main.java.ieseuropa;
 
+import java.util.Scanner;
+
 public class CadenaDeTexto {
 	
 	private static String repetirOcurrencia(String cadena) {
@@ -25,12 +27,37 @@ public class CadenaDeTexto {
 	private static int calcularLongitud(String cadena) {
 		return cadena.length();
 	}
+	
+	private static String eliminarAparienciasRepetidas(String texto1, String texto2) {
+		return texto1.toLowerCase().replaceAll(texto2.toLowerCase(), "").trim();
+	}
+	
+	private static String repetirStrings(String texto1, String texto2, int numero) {
+		String nuevoStr = "";
+		for (int i = 0; i < numero; i++) {
+			nuevoStr += "" + texto1 + texto2;
+		}
+		return nuevoStr;
+	}
+	
+	private static String textoMinuscula(String texto) {
+		return texto.toLowerCase();
+	}
+	
+	private static String textoMayuscula(String texto) {
+		return texto.toUpperCase();
+	}
 
 	public static void main(String[] args) {
 		String cadena = "Hola mundo";
 		System.out.println("La longitud de la cadena es de " + calcularLongitud(cadena) + " letras");
 		System.out.println("El numero de ocurrencias de la letra 'o' es: " + ocurrenciasLetra(cadena));
 		System.out.println(cadena + " se convierte en: " + repetirOcurrencia(cadena));
+		System.out.println(eliminarAparienciasRepetidas("Real Zaragoza es de la realeza", "Real"));
+		Scanner teclado = new Scanner(System.in);
+		System.out.println(repetirStrings("Mensaje 1","Mensaje 2",teclado.nextInt()));
+		System.out.println(textoMinuscula(cadena));
+		System.out.println(textoMayuscula(cadena));
 	}
 
 }
